058025a17ca4a28ed5e853087dcef656c0054213
vincentB299 <vincent.boardman@fdmgroup.com>
2024-06-27 16:47:30 +0100

    updating

--- Currency_WEB_API/Controllers/CurrencyTransferController.cs
+++ Currency_WEB_API/Controllers/CurrencyTransferController.cs
@@ -37,7 +37,7 @@ namespace Currency_WEB_API.Controllers
 
             if (currentUser == null)
             {
-                return Unauthorized(currentUserId + "not found");
+                return Unauthorized(currentUserId + "not found now");
             }
 
             var getPayeeId = await _userInformationServices.GetUserByTagService(transferAmountRequest.userTag);
